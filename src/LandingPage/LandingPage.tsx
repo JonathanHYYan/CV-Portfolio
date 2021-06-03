29582058c70b6b33cb0d4be856bda551fc225819
jyan1234 <jonathan.yan10@googlemail.com>
2021-06-03 17:39:47 +0100

    initial commit

---
+++ src/LandingPage/LandingPage.tsx
@@ -0,0 +1,13 @@
+import Card from "../UI/Card"
+import './LandingPage.scss';
+
+const LandingPage = () => {
+  return <section className='landing'>
+    <Card>
+      <h1>I'm John</h1>
+      <h2>a web developer</h2>
+    </Card>
+  </section>
+}
+
+export default LandingPage;
