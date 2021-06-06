04639fb3ff4a1b03ebcd5dfd543f0bcfa6fdf40c
jyan1234 <jonathan.yan10@googlemail.com>
2021-06-06 15:57:21 +0100

    Added class on scroll function

--- src/page/LandingPage/LandingPage.tsx
+++ src/page/LandingPage/LandingPage.tsx
@@ -1,12 +1,25 @@
-import './LandingPage.scss';
+import { FunctionComponent, useEffect, useState } from "react";
+import "./LandingPage.scss";
 
-const LandingPage = () => {
-  return <section className='landing night'>
-    <div className='name'>
-      <h1>I'm John</h1>
-      <h2>a web developer</h2>
-    </div>
-  </section>
-}
-// 
+const LandingPage: FunctionComponent = () => {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 1);
+    });
+  }, []);
+
+  let classes = "landing " + (scroll ? "night" : "");
+
+  return (
+    <section className={classes}>
+      <div className="name">
+        <h1>I'm John</h1>
+        <h2>a web developer</h2>
+      </div>
+    </section>
+  );
+};
+//
 export default LandingPage;
