68a517e0dfff2069b446d305db3ad9ff6b609fe0
jyan1234 <jonathan.yan10@googlemail.com>
2021-06-11 09:59:47 +0100

    added skills to dummy object, iterable jsx instead of hardcoded skill cards

--- src/page/About/Skills.tsx
+++ src/page/About/Skills.tsx
@@ -1,49 +1,67 @@
 import { useState, useEffect } from "react";
 import "./Skills.scss";
 
-const DummyData= [{},{},{},{},{},{}]
+const DummyData = [
+  {
+    id: "s6",
+    name: "Typescript",
+    num: 6,
+  },
+  {
+    id: "s5",
+    name: "React",
+    num: 5,
+  },
+  {
+    id: "s4",
+    name: "JavaScript",
+    num: 4,
+  },
+  {
+    id: "s3",
+    name: "SCSS",
+    num: 3,
+  },
+  {
+    id: "s2",
+    name: "CSS",
+    num: 2,
+  },
+  {
+    id: "s1",
+    name: "HTML",
+    num: 1,
+  },
+];
 
 const Skills = () => {
   const [scroll, setScroll] = useState(false);
 
-
-  useEffect(() =>{
-    const skillSection = document.getElementById('skills');
+  useEffect(() => {
+    const skillSection = document.getElementById("skills");
     const sectionTop = skillSection?.offsetTop;
     const sectionHeight = skillSection?.clientHeight;
-    const scrollBreakPoint = sectionHeight!/DummyData.length;
-    console.log(sectionTop);
-    console.log(sectionHeight);
-    console.log(scrollBreakPoint);
+    const scrollBreakPoint = sectionHeight! / DummyData.length;
 
     window.addEventListener("scroll", () => {
-      setScroll(window.scrollY > sectionTop! + scrollBreakPoint)
-    })
-  },[])
+      setScroll(window.scrollY > sectionTop! + scrollBreakPoint);
+    });
+  }, []);
 
-  const scrollCondition = scroll ? "skill-card skill-card-1 slide-out" : "skill-card skill-card-1";
+  const scrollCondition = scroll ? "slide-out" : "";
+  const skillList = DummyData.map((skill) => (
+    <div
+      className={`skill-card skill-card-${skill.num} ` + scrollCondition}
+      id={`skill${skill.num}`}
+    >
+      {skill.name}
+    </div>
+  ));
 
   return (
     <div className="skills" id="skills">
       <div className="image-container" id="imageContainer">
-        <div className="skill-card skill-card-6" id="skill6">
-          TypeScript
-        </div>
-        <div className="skill-card skill-card-5" id="skill5">
-          React
-        </div>
-        <div className="skill-card skill-card-4" id="skill4">
-          JS
-        </div>
-        <div className="skill-card skill-card-3" id="skill3">
-          SCSS
-        </div>
-        <div className="skill-card skill-card-2" id="skill2">
-          CSS
-        </div>
-        <div className={scrollCondition} id="skill1">
-          HTML
-        </div>
+        {skillList}
       </div>
     </div>
   );
