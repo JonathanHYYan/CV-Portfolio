cafa08fe90918468c5fd427ca775cf1d211c1810
jyan1234 <jonathan.yan10@googlemail.com>
2021-06-11 16:22:10 +0100

    staggered card transtions using multiple  useStates

--- src/page/About/Skills.tsx
+++ src/page/About/Skills.tsx
@@ -35,23 +35,55 @@ const DummyData = [
 ];
 
 const Skills = () => {
-  const [scroll, setScroll] = useState(false);
+  const [scroll1, setScroll1] = useState(false);
+  const [scroll2, setScroll2] = useState(false);
+  const [scroll3, setScroll3] = useState(false);
+  const [scroll4, setScroll4] = useState(false);
+  const [scroll5, setScroll5] = useState(false);
+  const [scroll6, setScroll6] = useState(false);
 
   useEffect(() => {
     const skillSection = document.getElementById("skills");
     const sectionTop = skillSection?.offsetTop;
     const sectionHeight = skillSection?.clientHeight;
     const scrollBreakPoint = sectionHeight! / DummyData.length;
+    const scrollStartModifyer = 0.6;
+    const scrollAmountModifyer = 0.9;
 
     window.addEventListener("scroll", () => {
-      setScroll(window.scrollY > sectionTop! + scrollBreakPoint);
+      setScroll1(
+        window.scrollY > scrollStartModifyer*sectionTop! + scrollAmountModifyer*(scrollBreakPoint * DummyData[0].num)
+      );
+      setScroll2(
+        window.scrollY > scrollStartModifyer*sectionTop! + scrollAmountModifyer*(scrollBreakPoint * DummyData[1].num)
+      );
+      setScroll3(
+        window.scrollY > scrollStartModifyer*sectionTop! + scrollAmountModifyer*(scrollBreakPoint * DummyData[2].num)
+      );
+      setScroll4(
+        window.scrollY > scrollStartModifyer*sectionTop! + scrollAmountModifyer*(scrollBreakPoint * DummyData[3].num)
+      );
+      setScroll5(
+        window.scrollY > scrollStartModifyer*sectionTop! + scrollAmountModifyer*(scrollBreakPoint * DummyData[4].num)
+      );
+      setScroll6(
+        window.scrollY > scrollStartModifyer*sectionTop! + scrollAmountModifyer*(scrollBreakPoint * DummyData[5].num)
+      );
     });
   }, []);
 
-  const scrollCondition = scroll ? "slide-out" : "";
-  const skillList = DummyData.map((skill) => (
+  const scrollCondition1 = scroll1 ? "slide-out" : "";
+  const scrollCondition2 = scroll2 ? "slide-out" : "";
+  const scrollCondition3 = scroll3 ? "slide-out" : "";
+  const scrollCondition4 = scroll4 ? "slide-out" : "";
+  const scrollCondition5 = scroll5 ? "slide-out" : "";
+  const scrollCondition6 = scroll6 ? "slide-out" : "";
+
+  const conditionHolder=[scrollCondition1,scrollCondition2,scrollCondition3,scrollCondition4,scrollCondition5,scrollCondition6]
+
+  const skillList = DummyData.map((skill,index) => (
     <div
-      className={`skill-card skill-card-${skill.num} ` + scrollCondition}
+      className={`skill-card skill-card-${skill.num} ` + conditionHolder[index]}
       id={`skill${skill.num}`}
     >
       {skill.name}
