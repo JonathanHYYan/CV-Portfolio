40d79d60dba4b470585e38dac953febfa6203a37
Jonathan Yan <jonathan.yan10@googlemail.com>
2023-06-29 01:27:35 +0100

    [CV-3] Added Carousel for Projects

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,50 @@
+export const slides = [
+  {
+    title: "MME Revise",
+    role: "Junior Front End Developer",
+    description:
+      "Front end design changes, mobile responsiveness adaptation to main website and online learning portal.",
+    stack: [
+      "php",
+      "laravel",
+      "html",
+      "css",
+      "js",
+      "jquery",
+      "bootstrap",
+      "vue.js",
+    ],
+  },
+  {
+    title: "MME 2",
+    role: "Junior Front End Developer",
+    description:
+      "Front end design changes, mobile responsiveness adaptation to main website and online learning portal.",
+    stack: [
+      "php",
+      "laravel",
+      "html",
+      "css",
+      "js",
+      "jquery",
+      "bootstrap",
+      "vue.js",
+    ],
+  },
+  {
+    title: "MME 3",
+    role: "Junior Front End Developer",
+    description:
+      "Front end design changes, mobile responsiveness adaptation to main website and online learning portal.",
+    stack: [
+      "php",
+      "laravel",
+      "html",
+      "css",
+      "js",
+      "jquery",
+      "bootstrap",
+      "vue.js",
+    ],
+  },
+];
---
+++ src/components/UI/CarouselSlide.tsx
@@ -0,0 +1,119 @@
+import {
+  ProjectTitle,
+  Slide,
+  ProjectRole,
+  ProjectDesc,
+  StackIcon,
+  Tech,
+  SlideSquare,
+} from "./Carouselstyles";
+import {
+  Css,
+  Html,
+  JavaScript,
+  Jq,
+  LaravelLogo,
+  PHP,
+  VueJs,
+  React,
+  Scss,
+  TypeScript,
+} from "../Main/TechStyles";
+
+type SlideObject = {
+  title: string;
+  role: string;
+  description: string;
+  stack: string[];
+};
+
+interface Props {
+  slide: SlideObject;
+  activeIndex: number;
+  index: number;
+}
+
+const CarouselSlide: React.FC<Props> = ({ slide, activeIndex, index }) => {
+  const techStack: any[] = [];
+
+  slide.stack.forEach((stack) => {
+    if (stack === "php") {
+      techStack.push(
+        <StackIcon>
+          <PHP />
+        </StackIcon>
+      );
+    } else if (stack === "laravel") {
+      techStack.push(
+        <StackIcon>
+          <LaravelLogo />
+        </StackIcon>
+      );
+    } else if (stack === "html") {
+      techStack.push(
+        <StackIcon>
+          <Html />
+        </StackIcon>
+      );
+    } else if (stack === "css") {
+      techStack.push(
+        <StackIcon>
+          <Css />
+        </StackIcon>
+      );
+    } else if (stack === "js") {
+      techStack.push(
+        <StackIcon>
+          <JavaScript />
+        </StackIcon>
+      );
+    } else if (stack === "jquery") {
+      techStack.push(
+        <StackIcon>
+          <Jq />
+        </StackIcon>
+      );
+    } else if (stack === "vue.js") {
+      techStack.push(
+        <StackIcon>
+          <VueJs />
+        </StackIcon>
+      );
+    } else if (stack === "react") {
+      techStack.push(
+        <StackIcon>
+          <React />
+        </StackIcon>
+      );
+    } else if (stack === "scss") {
+      techStack.push(
+        <StackIcon>
+          <Scss />
+        </StackIcon>
+      );
+    } else if (stack === "typescript") {
+      techStack.push(
+        <StackIcon>
+          <TypeScript />
+        </StackIcon>
+      );
+    }
+  });
+
+  return (
+    <Slide active={activeIndex === index ? true: false}>
+      <SlideSquare>
+        <ProjectTitle>{slide.title}</ProjectTitle>
+        <ProjectRole>Role: {slide.role}</ProjectRole>
+        <ProjectDesc>{slide.description}</ProjectDesc>
+      </SlideSquare>
+      <SlideSquare />
+      <SlideSquare>
+        <h2>Tech Stack</h2>
+        <Tech>{techStack}</Tech>
+      </SlideSquare>
+    </Slide>
+  );
+};
+
+export default CarouselSlide;
